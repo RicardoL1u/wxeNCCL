0956044921b52ed7685d753690aab1c975868c2c
wuxuaner <wuxuaner@infini-ai.com>
2024-04-30 15:28:22 +0800

    test NCCL

--- testNCCL.py
+++ testNCCL.py
@@ -54,12 +54,12 @@ def main():
     if num_gpus > 0:
         # Initialize distributed process group
         dist.init_process_group(backend='nccl')
-
+       
         rank = dist.get_rank()
         print(rank)
         world_size = dist.get_world_size()
         print(world_size)
-
+        torch.cuda.set_device(rank % torch.cuda.device_count())
         local_tensor = torch.tensor([rank]).float().cuda()
         dist.all_reduce(local_tensor, op=dist.ReduceOp.SUM)
 
